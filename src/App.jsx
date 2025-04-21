1de9e421eafa52864fe3afc7b77d5126aae21873
Nikolay <nikolay@MacBook-Air-Nikolay.local>
2025-04-21 22:30:16 +0300

    registration form on useStore

---
+++ src/App.jsx
@@ -0,0 +1,91 @@
+import { useRef, useState } from 'react';
+import './index.css';
+import { useStore, sendFormData } from './useStore.js';
+import { emailRegex, passwordRegex } from './checkValidation.js';
+
+export const App = () => {
+	const { getState, updateState } = useStore();
+	const [formError, setFormError] = useState('');
+
+	const submitButtonRef = useRef(null);
+
+	const { email, password, confirmPass } = getState();
+
+	const onChange = ({ target }) => updateState(target.name, target.value);
+
+	const onEmailBlur = ({ target }) => {
+		!emailRegex.test(target.value)
+			? setFormError(
+					`Неверный email адрес. Проверьте логин перед символом @ и домен почтового ящика!`,
+				)
+			: setFormError('');
+	};
+
+	const onPassBlur = ({ target }) => {
+		!passwordRegex.test(target.value)
+			? setFormError(
+					'Пароль должен быть не менее 8 символов, состоять из строчной, заглавной буквы латинского алфавита, цифры и спецсимвола!',
+				)
+			: setFormError('');
+	};
+
+	const onConfirmPassChange = ({ target }) => {
+		setFormError('');
+		target.value === password
+			? submitButtonRef.current.focus()
+			: updateState(target.name, target.value);
+	};
+
+	const onConfirmPassBlur = ({ target }) => {
+		password !== target.value ? setFormError(`Пароли не совпадают!`) : setFormError('');
+	};
+
+	const onSubmit = (event) => {
+		event.preventDefault();
+		sendFormData(getState());
+	};
+
+	return (
+		<div className="container">
+			<form onSubmit={onSubmit}>
+				<p className="header">Форма регистрации</p>
+
+				<label htmlFor="email">Email:</label>
+				<input
+					id="email"
+					name="email"
+					type="email"
+					placeholder="Ваш email"
+					value={email}
+					onChange={onChange}
+					onBlur={onEmailBlur}
+				/>
+				<label htmlFor="password">Пароль:</label>
+				<input
+					id="password"
+					name="password"
+					type="password"
+					placeholder="Пароль"
+					value={password}
+					onChange={onChange}
+					onBlur={onPassBlur}
+				/>
+				<label htmlFor="password2">Повторите пароль:</label>
+				<input
+					id="confirmPass"
+					name="confirmPass"
+					type="password"
+					placeholder="Повторите пароль"
+					value={confirmPass}
+					onChange={onConfirmPassChange}
+					onBlur={onConfirmPassBlur}
+				/>
+
+				{formError && <div className="errormsg">{formError}</div>}
+				<button ref={submitButtonRef} type="submit" disabled={!!formError}>
+					Зарегистрироваться
+				</button>
+			</form>
+		</div>
+	);
+};
---
+++ src/checkValidation.js
@@ -0,0 +1,2 @@
+export const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+export const passwordRegex = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[!@#$%^&*()]).{8,}$/;
